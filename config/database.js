5fe46de1ebc0aef6fad3cdbbdc5eab84a3e8173a
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-14 12:52:15 +0530

    connection function , may change later .

--- config/database.js
+++ config/database.js
@@ -10,7 +10,19 @@ require('dotenv').config()
 // making a function to establish connection // 
 
 const connectDb = async () => { 
+    // await is necessary as it is connecting to db // 
+
     try{
-        
+        await mongoose.connect(process.env.MONGO_URI)
+    
+    console.log("connected to db")
+    }
+    catch(err){
+        console.log("error while connecting  : ",err)
+        // exiting the connection process // s
+
+        process.exit(1)
     }
 }
+// exporting our db function //
+module.exports = connectDb;
