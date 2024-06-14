0a6bd56b7f0bddbb1d9ec74d09e5a8b1e9b61fce
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-14 20:17:37 +0530

    added user already exist demo

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -25,4 +25,21 @@ router.post('/register',async(req,res) => {
     // the validate it takes
     // the req body retrives the data from user and requests //
     const {name , email , password} = req.body
+
+    // first lets check if user exits or not 
+
+    try{
+        // lets intialize the user 
+        // we will check with email if user is already registered or not //
+        let user = User.findOne({email})
+
+        if(user){
+            return res.status(400).json({message:"User already exists , please login"})
+        }
+
+        // and he is new , for registering hash the password
+
+    }catch(error){
+        console.log(error)
+    }
 })
