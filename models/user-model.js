d2c9c5c37b2f008b0aa405f72d9ab28776d4a424
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-14 19:49:21 +0530

    completed our schema model

--- models/user-model.js
+++ models/user-model.js
@@ -1,6 +1,8 @@
 // creating a user-schema model which will be used for validation , verification etc purposes // 
 
+const { Timestamp } = require('mongodb')
 const mongoose = require('mongoose')
+const { default: isEmail } = require('validator/lib/isEmail')
 
 // creating the schema model // 
 
@@ -13,7 +15,36 @@ const UserSchema = mongoose.Schema({
         type:String, 
         // to add with message // 
         required:[true,"email is required"],
-        unique:true
+        unique:true,
+        // validating the email // 
+        validate:{
+            validator:validator.isEmail,
+            message:"please enter an valid email"
+        }
         
+        
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"]
+    },
+    // adding is user verified or not // 
+    isVerified:{
+        type:boolean,
+        default:false,
+
+    },
+    // a token for each user // 
+    isVerfiedToken:{
+        type:String
     }
+
+},{
+    Timestamp:true
 })
+// lets create our schema model 
+
+const Usermodel = mongoose.model('Usermodel',UserSchema);
+
+// exporting our Usermodel // 
+module.exports = Usermodel
