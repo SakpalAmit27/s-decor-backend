dbe7bba00ee95c43959a3abd537b00fc4ec2bf5e
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-14 12:45:53 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,13 @@
+// setting up index js main entry point // 
+
+
+//express / /
+
+const express = require('express')
+// env for data hide //
+const dotenv = require('dotenv')
+
+// cors for client // 
+
+const cors = require('cors')
+
