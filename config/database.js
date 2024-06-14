6deabca79386991b311a5e7bfeb39c4a24d184a6
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-14 12:49:06 +0530

    basic  boilerplate

---
+++ config/database.js
@@ -0,0 +1,16 @@
+// lets make a db connection . first importing mongoose // 
+
+const mongoose = require('mongoose')
+
+// for adding PORT // 
+require('dotenv').config() 
+
+
+
+// making a function to establish connection // 
+
+const connectDb = async () => { 
+    try{
+        
+    }
+}
