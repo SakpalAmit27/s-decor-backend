f54c6b6ac2fd90600e1c6bf809f3c8483f6a0984
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-14 20:05:57 +0530

    registering part 1

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -18,3 +18,11 @@ const bcrypt = require('bcryptjs')
 // jsonwebtoken // 
 
 const jwt = require('jsonwebtoken')
+
+// lets first register the user // 
+
+router.post('/register',async(req,res) => {
+    // the validate it takes
+    // the req body retrives the data from user and requests //
+    const {name , email , password} = req.body
+})
