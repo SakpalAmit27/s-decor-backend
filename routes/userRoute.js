5b2d829c9d49e60c8a3a7f6951228f1a75bccbc8
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-20 13:15:29 +0530

    added input validation along with requiest

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,8 +4,32 @@ const router = express.Router()
 
 const {userLogin,registerUser,verifyEmail} = require('../controllers/user-controller')
 
+// adding our authMiddleware // 
+const authMiddleware = require('../middlewares/auth-middleware')
+
+const validationMiddleware = require('../middlewares/validation-middleware')
+const { check } = require('express-validator')
+
 router.post('/login',userLogin)
-router.post('/signup',registerUser)
+// router.post('/signup',registerUser)
+
+// adding validation to input fields with made middlewar e// 
+
+router.post(
+    // using express-validator to check //
+    '/register',[
+        check('name','name is required').not().isEmpty(),
+        check('email','email is required').isEmail(),
+        check('password','password minimum must be 6').isLength({min : 6})
+    ],
+
+    // adding our middlwares here //
+
+
+    validationMiddleware,
+    registerUser
+)
+
 router.get('/verify/:token',verifyEmail)
 module.exports = router
 
