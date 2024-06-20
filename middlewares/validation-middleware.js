d59d333b36844e88fd6e699d0d44a3772bd89de9
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-20 13:15:06 +0530

    fixed isEmpty error

--- middlewares/validation-middleware.js
+++ middlewares/validation-middleware.js
@@ -10,8 +10,8 @@ const validationMiddleware = async (req,res,next) => {
 
     const errors = validationResult(req)
 
-    if(!errors.isEmpty){
-        return res.status(400).json({errors : errors.array()})
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors})
     }
 
     next();
