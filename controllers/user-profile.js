2e37547d257490ad11117e795a168b09753b6e06
Sakpal Amit <amitsakpal744@gmail.com>
2024-06-20 16:55:19 +0530

    added userprofile to get the user in json

--- controllers/user-profile.js
+++ controllers/user-profile.js
@@ -1,1 +1,28 @@
 // a controller for user profile management  eg : getting the user profile //// 
+
+// important things getting the user-model ..//
+
+const User = require('../models/user-model')
+
+const getUserProfile = async (req,res) => {
+
+    // first lets check the user if he is actually existing or not // 
+
+    // this time we are finding with password // 
+
+    try{
+        const user = await User.findOne(req.body).select('-password')
+
+        // if he does not exist // 
+
+        if(!user){
+            res.status(404).json({message:"user not found"})
+        }
+        // if he does and validation is success show the user in json for frontend // 
+
+        res.json(user)
+    }catch(error){
+        console.error(message.error);
+        res.status(500).send('Server error')
+    }
+}
